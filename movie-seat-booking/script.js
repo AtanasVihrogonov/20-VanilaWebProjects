f8c3679cd3a4d1532f748af3017d72352f86634e
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-04-17 12:07:56 +0100

    Set to LS

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -5,11 +5,25 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 
+populateUI();
+
+// Save selected index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
 // Update total and count
-function updateSelectedCount() {
+function updateSelectedCountAndTotal() {
   // 1.Get selected seats
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  // Copy selected seats into an array & Map through that array & Return a new array of indexes
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  //console.log(seatsIndex);
 
+  // Set to LS
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+ 
   // 2.Get the length of selected seats
   const selectedSeatsCount = selectedSeats.length;
 
@@ -20,10 +34,30 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 };
 
+// Get data from localstorage and populateUI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  //console.log(selectedSeats);
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+};
+
 // Movie select event
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
-  updateSelectedCount();
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCountAndTotal();
 });
 
 // Seat click event
@@ -33,6 +67,10 @@ container.addEventListener('click', e => {
     // Add selected class
     e.target.classList.toggle('selected');
     
-    updateSelectedCount();
+    updateSelectedCountAndTotal();
   }
 });
+
+// Initial count and total set
+updateSelectedCountAndTotal();
+
