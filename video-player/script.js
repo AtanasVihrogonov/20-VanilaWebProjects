947555d6f92b3e42e40ef122ca840fd6464cdb1a
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-04-20 11:21:19 +0100

    update progress

--- video-player/script.js
+++ video-player/script.js
@@ -1,41 +1,63 @@
 const video = document.getElementById('video');
 const play = document.getElementById('play');
-const stop = document.getElementById('stops');
+const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
-// Play and pause video
+// Play & pause video
 function toggleVideoStatus() {
-  return true;
-};
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
-// Update play/pouse icon
+// update play/pause icon
 function updatePlayIcon() {
-  return true;
-};
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
 
 // Update progress & timestamp
 function updateProgress() {
-  return true;
-};
+  progress.value = (video.currentTime / video.duration) * 100;
 
-// Stop video icon
-function stopVideo() {
-  return true;
-};
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
 
-// Set video  time to progress
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60)
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
+}
+
+// Set video time to progress
 function setVideoProgress() {
-  return true;
-};
+  video.currentTime = (+progress.value * video.duration) / 100;
+}
 
+// Stop video
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
 
 // Event listeners
 video.addEventListener('click', toggleVideoStatus);
-video.addEventListener('pouse', updatePlayIcon);
+video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
-video.addEventListener('timestamp', updateProgress);
+video.addEventListener('timeupdate', updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
-stops.addEventListener('click', stopVideo);
+stop.addEventListener('click', stopVideo);
 progress.addEventListener('change', setVideoProgress);
