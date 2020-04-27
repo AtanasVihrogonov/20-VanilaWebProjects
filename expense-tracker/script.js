4f61f50682e4751e2446ee589d33edc639eaa49e
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-04-27 23:51:18 +0100

    Display balance, income & expense

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -6,17 +6,69 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-const dummyTransaction = [
-  { id=1, text: 'Online Course', amount: -20 },
-  { id=2, text: 'Work', amount: 300 },
-  { id=3, text: 'Phone screen', amount: -10 },
-  { id=4, text: 'Cashback', amount: 70 }
+const dummyTransactions = [
+  { id: 1, text: 'Online Course', amount: -20 },
+  { id: 2, text: 'Work', amount: 300 },
+  { id: 3, text: 'Phone Screen', amount: -10 },
+  { id: 4, text: 'Cashback', amount: 70 }
 ];
 
-let transaction = dummyTransaction;
+let transactions = dummyTransactions;
 
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
   // 1.Get sign
-  
-}
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  // 2.Create new element
+  const item = document.createElement('li');
+
+  // 3.Add class based on value
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  item.innerHTML = `
+    ${transaction.text} 
+    <span>${sign}${Math.abs(transaction.amount)}</span> 
+    <button class="delete-btn">x</button>
+  `;
+
+  list.appendChild(item);
+};
+
+// Update the balance, income and expense
+function updateValues() {
+  // Create an array just for amounts
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  // Calculate total amount
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item)
+    .toFixed(2));
+
+  const expense = (amounts
+    .filter(item => item < 0)
+    .reduce((acc, item) => (acc += item), 0) * -1)
+    .toFixed(2);
+
+  // console.log(expense);
+  // console.log(income);
+  // console.log(total);
+  // console.log(amounts);
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+};
+
+// Init app
+function init() {
+  list.innerHTML = '';
+
+  transactions.forEach(addTransactionDOM);
+
+  updateValues();
+};
+
+init();
