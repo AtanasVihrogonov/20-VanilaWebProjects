a9f4d343b1d167d5e68ccb1a801b470c0738a449
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-05-01 16:29:20 +0100

    Add infinite scrolling & Filter fetched posts

--- blog-post/script.js
+++ blog-post/script.js
@@ -3,7 +3,7 @@ const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
 // Global variables
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 // Fetch posts from API
@@ -19,8 +19,76 @@ async function getPosts() {
 async function showPosts() {
   const posts = await getPosts();
 
-  console.log(posts);
+  // Display into DOM
+  posts.forEach(post => {
+    // Create div
+    const postEl = document.createElement('div');
+
+    // Add post class
+    postEl.classList.add('post');
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+    `;
+
+    postsContainer.appendChild(postEl);
+  });
 }
+
+// Show loader & fetch more post
+function showLoading() {
+  // Add the class of show
+  loading.classList.add('show');
+  
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 200);
+
+  }, 1000);
+};
+
+// Filter post by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+
+  // Get all of the post
+  const posts = document.querySelectorAll('.post');
+
+  // Loop through the node list array
+  posts.forEach(post => {
+    // Get title of the body
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    // Match it to the term
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+};
 
 // Show initial posts
 showPosts();
+
+window.addEventListener('scroll', () => {
+  // console.log(document.documentElement.scrollTop);
+  const { scrollTop, scrollHeight , clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    // Show the loader
+    showLoading();
+    // Fetch the rest of the post
+  }       
+});
+
+filter.addEventListener('input', filterPosts);
+
