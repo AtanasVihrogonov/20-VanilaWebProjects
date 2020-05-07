85dbaebba1a999cefeb45ccdd689fe4d4a5fbcfd
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-05-07 20:09:53 +0100

    Get & Display song lyrics

--- lyrics-search/script.js
+++ lyrics-search/script.js
@@ -72,6 +72,19 @@ async function getMoreSongs(url) {
   showData(data);
 };
 
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+  result.innerHTML = `
+    <h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>
+  `;
+
+  more.innerHTML = '';
+};
 
 // Event listener
 form.addEventListener('submit', (e) => {
@@ -85,3 +98,15 @@ form.addEventListener('submit', (e) => {
     searchSongs(searchTerm);
   }
 });
+
+// Get Lyrics button click
+result.addEventListener('click', e => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle);
+  }
+})
