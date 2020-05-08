941acc54eff53fbbd7cd57c70a759f758491d72b
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-05-08 14:18:14 +0100

    Draw Ball, Paddle & Score

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -1,6 +1,65 @@
 const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let score = 0;
+
+// Create ball props
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4,
+};
+
+// Create paddle props
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0,
+  dy: 0,
+};
+
+// Draw ball on canvas
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2); // Create ball
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+};
+
+// Draw paddle on canvas
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+};
+
+// Draw evething
+function draw() {
+  drawBall();
+  drawPaddle();
+  drawScore();
+};
+
+// Draw score on canvas
+function drawScore() {
+  ctx.font = '20px Combria',
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+};
+
+
+draw();
 
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
