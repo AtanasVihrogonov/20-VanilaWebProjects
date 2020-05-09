0da893395c8debb116a2d3bfbac414e5f24b98ba
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-05-09 14:34:04 +0100

    Lose & Reset Game

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -143,9 +143,33 @@ function moveBall() {
         ) {
           ball.dy *= -1;
           brick.visible = false;
+
+          increaseScore();
         }
       }
     })
+  })
+
+  // Hit bottom wall - lose
+  if (ball.y + ball.size > canvas.height) {
+    showAllBricks();
+    score = 0;
+  }
+};
+
+// Increase score
+function increaseScore() {
+  score++;
+
+  if (score % (brickRowCount * brickRowCount) === 0) {
+    showAllBricks();
+  }
+};
+
+// Make all bricks apper
+function showAllBricks() {
+  bricks.forEach(column => {
+    column.forEach(brick => (brick.visible = true));
   })
 };
 
